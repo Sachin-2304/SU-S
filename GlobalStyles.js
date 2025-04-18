df237d18dbe3c675b03ddcea462fafb926cde71f
Sachin-2304 <sachin.kumar.870sk@gmail.com>
2025-04-18 11:17:17 +0530

    Create GlobalStyles.js

---
+++ GlobalStyles.js
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: ${props => props.theme.typography.fontFamily};
+    background: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text};
+    line-height: 1.6;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 600;
+    line-height: 1.2;
+  }
+
+  a {
+    color: ${props => props.theme.colors.accent};
+    text-decoration: none;
+    transition: color 0.3s ease;
+
+    &:hover {
+      color: ${props => props.theme.colors.primary};
+    }
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    outline: none;
+    font-family: inherit;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
